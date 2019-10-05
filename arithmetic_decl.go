65c51fb856830436c2620626e48141debdc41673
kilic <kiliconu@itu.edu.tr>
2019-10-05 23:24:57 +0300

    add noescape tag

--- arithmetic_decl.go
+++ arithmetic_decl.go
@@ -1,15 +1,22 @@
 package bls
 
+//go:noescape
 func add(c, a, b *Fe)
 
+//go:noescape
 func addn(a, b *Fe) uint64
 
+//go:noescape
 func sub(c, a, b *Fe)
 
+//go:noescape
 func subn(a, b *Fe) uint64
 
+//go:noescape
 func neg(c, a *Fe)
 
+//go:noescape
 func double(c, a *Fe)
 
+//go:noescape
 func montmul(c, a, b *Fe)
