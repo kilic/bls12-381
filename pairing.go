65ecb58439f29d8bb35af8b2276f1da7b2d8b2c7
Sait Imamoglu <nuggez@gmail.com>
2020-03-11 00:49:09 +0300

    Refactor pairing and add more tests

--- pairing.go
+++ pairing.go
@@ -1,12 +1,20 @@
 package bls
 
+type Pairs = []Pair
+
+type Pair struct {
+	g1 PointG1
+	g2 PointG2
+}
+
 type BLSPairingEngine struct {
-	G1   *G1
-	G2   *G2
-	fp12 *fp12
-	fp2  *fp2
-	t2   [10]*fe2
-	t12  [9]fe12
+	G1    *G1
+	G2    *G2
+	fp12  *fp12
+	fp2   *fp2
+	t2    [10]*fe2
+	t12   [9]fe12
+	pairs Pairs
 }
 
 func NewBLSPairingEngine() *BLSPairingEngine {
@@ -30,6 +38,7 @@ func NewBLSPairingEngine() *BLSPairingEngine {
 	}
 }
 
+// Adaptation of Formula 3 in https://eprint.iacr.org/2010/526.pdf
 func (e *BLSPairingEngine) doublingStep(coeff *[3]fe2, r *PointG2) {
 	fp2 := e.fp2
 	t := e.t2
@@ -63,6 +72,7 @@ func (e *BLSPairingEngine) doublingStep(coeff *[3]fe2, r *PointG2) {
 	fp2.neg(&coeff[2], t[6])
 }
 
+// Algorithm 12 in https://eprint.iacr.org/2010/526.pdf
 func (e *BLSPairingEngine) additionStep(coeff *[3]fe2, r, q *PointG2) {
 	fp2 := e.fp2
 	t := e.t2
@@ -93,7 +103,8 @@ func (e *BLSPairingEngine) additionStep(coeff *[3]fe2, r, q *PointG2) {
 	fp2.copy(&coeff[2], t[1])
 }
 
-func (e *BLSPairingEngine) prepare(ellCoeffs *[70][3]fe2, twistPoint *PointG2) {
+// Algorithm 5 in  https://eprint.iacr.org/2019/077.pdf
+func (e *BLSPairingEngine) preCompute(ellCoeffs *[70][3]fe2, twistPoint *PointG2) {
 	if e.G2.IsZero(twistPoint) {
 		return
 	}
@@ -111,48 +122,42 @@ func (e *BLSPairingEngine) prepare(ellCoeffs *[70][3]fe2, twistPoint *PointG2) {
 	}
 }
 
-// notice that this function expects: len(points) == len(twistPoints)
-func (e *BLSPairingEngine) millerLoop(f *fe12, points []PointG1, twistPoints []PointG2) {
-	for i := 0; i <= len(points)-1; i++ {
-		e.G1.Affine(&points[i])
-		e.G2.Affine(&twistPoints[i])
+func (e *BLSPairingEngine) millerLoop(f *fe12, pairs Pairs) {
+	for i := 0; i <= len(pairs)-1; i++ {
+		e.G1.Affine(&pairs[i].g1)
+		e.G2.Affine(&pairs[i].g2)
 	}
-	ellCoeffs := make([][70][3]fe2, len(points))
-	for i := 0; i < len(points); i++ {
-		if !e.G1.IsZero(&points[i]) && !e.G2.IsZero(&twistPoints[i]) {
-			e.prepare(&ellCoeffs[i], &twistPoints[i])
-		}
+	ellCoeffs := make([][70][3]fe2, len(pairs))
+	for i := 0; i < len(pairs); i++ {
+		e.preCompute(&ellCoeffs[i], &pairs[i].g2)
 	}
 	fp12 := e.fp12
 	fp2 := e.fp2
 	t := e.t2
 	fp12.copy(f, fp12.one())
-	// first iteration
-	// x.BitLen() - 2
-	// w/o squaring f
-	for i := 0; i <= len(points)-1; i++ {
-		fp2.mulByFq(t[0], &ellCoeffs[i][0][2], &points[i][1])
-		fp2.mulByFq(t[1], &ellCoeffs[i][0][1], &points[i][0])
+	for i := 0; i <= len(pairs)-1; i++ {
+		fp2.mulByFq(t[0], &ellCoeffs[i][0][2], &pairs[i].g1[1])
+		fp2.mulByFq(t[1], &ellCoeffs[i][0][1], &pairs[i].g1[0])
 		fp12.mulBy014Assign(f, &ellCoeffs[i][0][0], t[1], t[0])
 	}
-	for i := 0; i <= len(points)-1; i++ {
-		fp2.mulByFq(t[0], &ellCoeffs[i][1][2], &points[i][1])
-		fp2.mulByFq(t[1], &ellCoeffs[i][1][1], &points[i][0])
+	for i := 0; i <= len(pairs)-1; i++ {
+		fp2.mulByFq(t[0], &ellCoeffs[i][1][2], &pairs[i].g1[1])
+		fp2.mulByFq(t[1], &ellCoeffs[i][1][1], &pairs[i].g1[0])
 		fp12.mulBy014Assign(f, &ellCoeffs[i][1][0], t[1], t[0])
 	}
 	j := 2
 	for i := int(x.BitLen() - 3); i >= 0; i-- {
 		fp12.square(f, f)
-		for i := 0; i <= len(points)-1; i++ {
-			fp2.mulByFq(t[0], &ellCoeffs[i][j][2], &points[i][1])
-			fp2.mulByFq(t[1], &ellCoeffs[i][j][1], &points[i][0])
+		for i := 0; i <= len(pairs)-1; i++ {
+			fp2.mulByFq(t[0], &ellCoeffs[i][j][2], &pairs[i].g1[1])
+			fp2.mulByFq(t[1], &ellCoeffs[i][j][1], &pairs[i].g1[0])
 			fp12.mulBy014Assign(f, &ellCoeffs[i][j][0], t[1], t[0])
 		}
 		if x.Bit(i) != 0 {
 			j++
-			for i := 0; i <= len(points)-1; i++ {
-				fp2.mulByFq(t[0], &ellCoeffs[i][j][2], &points[i][1])
-				fp2.mulByFq(t[1], &ellCoeffs[i][j][1], &points[i][0])
+			for i := 0; i <= len(pairs)-1; i++ {
+				fp2.mulByFq(t[0], &ellCoeffs[i][j][2], &pairs[i].g1[1])
+				fp2.mulByFq(t[1], &ellCoeffs[i][j][1], &pairs[i].g1[0])
 				fp12.mulBy014Assign(f, &ellCoeffs[i][j][0], t[1], t[0])
 			}
 		}
@@ -170,6 +175,7 @@ func (e *BLSPairingEngine) exp(c, a *fe12) {
 func (e *BLSPairingEngine) finalExp(f *fe12) {
 	fp12 := e.fp12
 	t := e.t12
+	// easy part
 	fp12.frobeniusMap(&t[0], f, 6)
 	fp12.inverse(&t[1], f)
 	fp12.mul(&t[2], &t[0], &t[1])
@@ -178,6 +184,7 @@ func (e *BLSPairingEngine) finalExp(f *fe12) {
 	fp12.mulAssign(&t[2], &t[1])
 	fp12.cyclotomicSquare(&t[1], &t[2])
 	fp12.conjugate(&t[1], &t[1])
+	// hard but tricky part
 	e.exp(&t[3], &t[2])
 	fp12.cyclotomicSquare(&t[4], &t[3])
 	fp12.mul(&t[5], &t[1], &t[3])
@@ -201,14 +208,25 @@ func (e *BLSPairingEngine) finalExp(f *fe12) {
 	fp12.mul(f, &t[3], &t[4])
 }
 
-func (e *BLSPairingEngine) pair(f *fe12, points []PointG1, twistPoints []PointG2) {
-	e.millerLoop(f, points, twistPoints)
+func (e *BLSPairingEngine) pair(pairs Pairs) *fe12 {
+	f := e.fp12.one()
+	if len(pairs) == 0 {
+		return f
+	}
+	var newPairs Pairs
+	for i := 0; i < len(pairs); i++ {
+		if !e.G1.IsZero(&pairs[i].g1) && !e.G2.IsZero(&pairs[i].g2) {
+			newPairs = append(newPairs, pairs[i])
+		}
+	}
+	if len(newPairs) == 0 {
+		return f
+	}
+	e.millerLoop(f, newPairs)
 	e.finalExp(f)
+	return f
 }
 
-func (e *BLSPairingEngine) PairingCheck(points []PointG1, twistPoints []PointG2) bool {
-	gt, f := e.fp12, &fe12{}
-	e.millerLoop(f, points, twistPoints)
-	e.finalExp(f)
-	return gt.equal(gt.one(), f)
+func (e *BLSPairingEngine) Check(pairs Pairs) bool {
+	return e.fp12.equal(e.fp12.one(), e.pair(pairs))
 }
--- pairing_test.go
+++ pairing_test.go
@@ -7,102 +7,154 @@ import (
 )
 
 func TestPairing(t *testing.T) {
-	e := NewBLSPairingEngine()
+	bls := NewBLSPairingEngine()
+	g1Zero, g2Zero, g1One, g2One := bls.G1.Zero(), bls.G2.Zero(), bls.G1.One(), bls.G2.One()
+	generatorPair := Pairs{Pair{*g1One, *g2One}}
+	expected, err := bls.fp12.fromBytes(
+		fromHex(48,
+			"0x0f41e58663bf08cf068672cbd01a7ec73baca4d72ca93544deff686bfd6df543d48eaa24afe47e1efde449383b676631",
+			"0x04c581234d086a9902249b64728ffd21a189e87935a954051c7cdba7b3872629a4fafc05066245cb9108f0242d0fe3ef",
+			"0x03350f55a7aefcd3c31b4fcb6ce5771cc6a0e9786ab5973320c806ad360829107ba810c5a09ffdd9be2291a0c25a99a2",
+			"0x11b8b424cd48bf38fcef68083b0b0ec5c81a93b330ee1a677d0d15ff7b984e8978ef48881e32fac91b93b47333e2ba57",
+			"0x06fba23eb7c5af0d9f80940ca771b6ffd5857baaf222eb95a7d2809d61bfe02e1bfd1b68ff02f0b8102ae1c2d5d5ab1a",
+			"0x19f26337d205fb469cd6bd15c3d5a04dc88784fbb3d0b2dbdea54d43b2b73f2cbb12d58386a8703e0f948226e47ee89d",
+			"0x018107154f25a764bd3c79937a45b84546da634b8f6be14a8061e55cceba478b23f7dacaa35c8ca78beae9624045b4b6",
+			"0x01b2f522473d171391125ba84dc4007cfbf2f8da752f7c74185203fcca589ac719c34dffbbaad8431dad1c1fb597aaa5",
+			"0x193502b86edb8857c273fa075a50512937e0794e1e65a7617c90d8bd66065b1fffe51d7a579973b1315021ec3c19934f",
+			"0x1368bb445c7c2d209703f239689ce34c0378a68e72a6b3b216da0e22a5031b54ddff57309396b38c881c4c849ec23e87",
+			"0x089a1c5b46e5110b86750ec6a532348868a84045483c92b7af5af689452eafabf1a8943e50439f1d59882a98eaa0170f",
+			"0x1250ebd871fc0a92a7b2d83168d0d727272d441befa15c503dd8e90ce98db3e7b6d194f60839c508a84305aaca1789b6",
+		))
+	if err != nil {
+		t.Fatal(err)
+	}
 	t.Run("Single Expected", func(t *testing.T) {
-		G, H := &PointG1{}, &PointG2{}
-		e.G1.Copy(G, &g1One)
-		e.G2.Copy(H, &g2One)
-		P1, P2 := []PointG1{*G}, []PointG2{*H}
-		f1 := &fe12{}
-		e.pair(f1, P1, P2)
-		f2, err := e.fp12.fromBytes(
-			fromHex(48,
-				"0x0f41e58663bf08cf068672cbd01a7ec73baca4d72ca93544deff686bfd6df543d48eaa24afe47e1efde449383b676631",
-				"0x04c581234d086a9902249b64728ffd21a189e87935a954051c7cdba7b3872629a4fafc05066245cb9108f0242d0fe3ef",
-				"0x03350f55a7aefcd3c31b4fcb6ce5771cc6a0e9786ab5973320c806ad360829107ba810c5a09ffdd9be2291a0c25a99a2",
-				"0x11b8b424cd48bf38fcef68083b0b0ec5c81a93b330ee1a677d0d15ff7b984e8978ef48881e32fac91b93b47333e2ba57",
-				"0x06fba23eb7c5af0d9f80940ca771b6ffd5857baaf222eb95a7d2809d61bfe02e1bfd1b68ff02f0b8102ae1c2d5d5ab1a",
-				"0x19f26337d205fb469cd6bd15c3d5a04dc88784fbb3d0b2dbdea54d43b2b73f2cbb12d58386a8703e0f948226e47ee89d",
-				"0x018107154f25a764bd3c79937a45b84546da634b8f6be14a8061e55cceba478b23f7dacaa35c8ca78beae9624045b4b6",
-				"0x01b2f522473d171391125ba84dc4007cfbf2f8da752f7c74185203fcca589ac719c34dffbbaad8431dad1c1fb597aaa5",
-				"0x193502b86edb8857c273fa075a50512937e0794e1e65a7617c90d8bd66065b1fffe51d7a579973b1315021ec3c19934f",
-				"0x1368bb445c7c2d209703f239689ce34c0378a68e72a6b3b216da0e22a5031b54ddff57309396b38c881c4c849ec23e87",
-				"0x089a1c5b46e5110b86750ec6a532348868a84045483c92b7af5af689452eafabf1a8943e50439f1d59882a98eaa0170f",
-				"0x1250ebd871fc0a92a7b2d83168d0d727272d441befa15c503dd8e90ce98db3e7b6d194f60839c508a84305aaca1789b6",
-			))
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !e.fp12.equal(f1, f2) {
+		pairs := Pairs{Pair{*g1One, *g2One}}
+		f1 := bls.pair(pairs)
+		if !bls.fp12.equal(f1, expected) {
 			t.Fatal("bad pairing")
+		}
+	})
+
+	randomPair := func() (Pair, *big.Int) {
+		var pair Pair
+		a, err := rand.Int(rand.Reader, q)
+		if err != nil {
+			panic(err)
+		}
+		bls.G1.MulScalar(&pair.g1, g1One, a)
+		bls.G1.Affine(&pair.g1)
+
+		b, err := rand.Int(rand.Reader, q)
+		if err != nil {
+			panic(err)
+		}
+		bls.G2.MulScalar(&pair.g2, g2One, b)
+		bls.G2.Affine(&pair.g2)
+		c := new(big.Int).Mul(a, b)
+		return pair, c
+	}
+
+	t.Run("Multi Pair", func(t *testing.T) {
+		pairSize := 50
+		pairs := make(Pairs, pairSize)
+		acc := new(big.Int)
+		var c *big.Int
+		for i := 0; i < pairSize; i++ {
+			pairs[i], c = randomPair()
+			acc.Add(acc, c)
+		}
+		f0 := bls.pair(pairs)
+		f1 := bls.pair(generatorPair)
+		bls.fp12.exp(f1, f1, acc)
+		if !bls.fp12.equal(f0, f1) {
+			t.Fatalf("bad pairing")
+		}
+	})
+
+	t.Run("Bilinearity", func(t *testing.T) {
+		var f1, f2 *fe12
+		// e(a*G1, b*G2) = e(G1, G2)^c
+		randomPair, c := randomPair()
+		pair := Pairs{randomPair}
+		t.Run("Bilinearity-1", func(t *testing.T) {
+			f1 = bls.pair(generatorPair)
+			f2 = bls.pair(pair)
+			bls.fp12.exp(f1, f1, c)
+			if !bls.fp12.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+		// e(a*G1, b*G2) = e(c*G1, G2)
+		t.Run("Bilinearity-2", func(t *testing.T) {
+			bls.G1.MulScalar(&generatorPair[0].g1, g1One, c)
+			f2 = bls.pair(generatorPair)
+			if !bls.fp12.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+		// e(a*G1, b*G2) = e(G1, c*G2)
+		t.Run("Bilinearity-3", func(t *testing.T) {
+			bls.G1.Copy(&generatorPair[0].g1, g1One)
+			bls.G2.MulScalar(&generatorPair[0].g2, g2One, c)
+			f2 = bls.pair(generatorPair)
+			if !bls.fp12.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+	})
+
+	t.Run("Non-Degeneracy", func(t *testing.T) {
+		pairs := Pairs{
+			Pair{*g1Zero, *g2One},
+			Pair{*g1One, *g2Zero},
+			Pair{*g1Zero, *g2Zero},
+		}
+		if !bls.fp12.equal(bls.fp12.one(), bls.pair(pairs)) {
+			t.Errorf("bad pairing")
+		}
+		pairs = Pairs{
+			Pair{*g1Zero, *g2One},
+			Pair{*g1One, *g2Zero},
+			Pair{*g1Zero, *g2Zero},
+			Pair{*g1One, *g2One},
+		}
+		if !bls.fp12.equal(expected, bls.pair(pairs)) {
+			t.Errorf("bad pairing")
+		}
+	})
+
+	t.Run("Pairing Check", func(t *testing.T) {
+		g1NegOne := bls.G1.NegativeOne()
+		g2NegOne := bls.G2.New()
+		bls.G2.Neg(g2NegOne, g2One)
+		pairs := Pairs{
+			Pair{*g1One, *g2One},
+			Pair{*g1NegOne, *g2One},
+			Pair{*g1One, *g2One},
+			Pair{*g1One, *g2NegOne},
+		}
+		if !bls.fp12.equal(bls.fp12.one(), bls.pair(pairs)) {
+			t.Errorf("bad pairing")
 		}
 	})
 }
 
-func TestPairingMulti(t *testing.T) {
-	e := NewBLSPairingEngine()
-	G := &PointG1{}
-	H := &PointG2{}
-	e.G1.Copy(G, &g1One)
-	e.G2.Copy(H, &g2One)
-	g1RandPoint := func() (*PointG1, *big.Int) {
-		s, err := rand.Int(rand.Reader, q)
-		if err != nil {
-			panic(err)
-		}
-		p := e.G1.MulScalar(&PointG1{}, G, s)
-		e.G1.Affine(p)
-		return p, s
-	}
-	g2RandPoint := func() (*PointG2, *big.Int) {
-		s, err := rand.Int(rand.Reader, q)
-		if err != nil {
-			panic(err)
-		}
-		p := e.G2.MulScalar(&PointG2{}, H, s)
-		e.G2.Affine(p)
-		return p, s
-	}
-	pairSize := 50
-	points := make([]PointG1, pairSize)
-	twistPoints := make([]PointG2, pairSize)
-	acc := new(big.Int)
-	var f0, f1 fe12
-	for i := 0; i < pairSize; i++ {
-		aG, a := g1RandPoint()
-		bH, b := g2RandPoint()
-		e.G1.Copy(&points[i], aG)
-		e.G2.Copy(&twistPoints[i], bH)
-		acc.Add(acc, new(big.Int).Mul(a, b))
-	}
-	e.pair(&f0, points, twistPoints)
-	e.pair(&f1, []PointG1{*G}, []PointG2{*H})
-	e.fp12.exp(&f1, &f1, acc)
-	if !e.fp12.equal(&f0, &f1) {
-		t.Fatalf("bad pairing")
-	}
-}
-
 func BenchmarkPairing(t *testing.B) {
-	e := NewBLSPairingEngine()
-	G := &PointG1{}
-	H := &PointG2{}
-	e.G1.Copy(G, &g1One)
-	e.G2.Copy(H, &g2One)
-	points := []PointG1{*G}
-	twistPoints := []PointG2{*H}
-	f1 := &fe12{}
+	bls := NewBLSPairingEngine()
+	g1One, g2One := bls.G1.One(), bls.G2.One()
 	t.ResetTimer()
+	generatorPair := Pairs{Pair{*g1One, *g2One}}
 	for i := 0; i < t.N; i++ {
-		e.pair(f1, points, twistPoints)
+		bls.pair(generatorPair)
 	}
 }
 
 func BenchmarkFinalExp(t *testing.B) {
-	e := NewBLSPairingEngine()
-	a, _ := e.fp12.rand(rand.Reader)
+	bls := NewBLSPairingEngine()
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
-		e.finalExp(a)
+		bls.finalExp(bls.fp12.one())
 	}
+
 }
