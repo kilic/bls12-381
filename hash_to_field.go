1d7939d2cd92a7b73c71042d8b7e06b88ddb22f0
kilic <kiliconu@itu.edu.tr>
2020-04-27 16:37:33 +0300

    update for hashing to elliptic curves standart v7
    field element sign changes to le
    
    add hash to field sha256 xmd variant
    
    add new end points hash to curve and encode to curve for sha256 xmd variant

--- hash_to_field.go
+++ hash_to_field.go
@@ -1,17 +1,16 @@
 package bls12381
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"hash"
 )
 
-func hashToField(hasher hash.Hash, msg []byte, domain []byte, count int) ([]*fe, error) {
-	randBytes, err := expandMsg(hasher, msg, domain, count*64)
+func hashToFpXMDSHA256(msg []byte, domain []byte, count int) ([]*fe, error) {
+	randBytes, err := expandMsgSHA256XMD(msg, domain, count*64)
 	if err != nil {
 		return nil, err
 	}
 	els := make([]*fe, count)
-	// var err error
 	for i := 0; i < count; i++ {
 		els[i], err = from64Bytes(randBytes[i*64 : (i+1)*64])
 		if err != nil {
@@ -21,29 +20,28 @@ func hashToField(hasher hash.Hash, msg []byte, domain []byte, count int) ([]*fe,
 	return els, nil
 }
 
-func expandMsg(h hash.Hash, msg []byte, domain []byte, outLen int) ([]byte, error) {
+func expandMsgSHA256XMD(msg []byte, domain []byte, outLen int) ([]byte, error) {
+	h := sha256.New()
 	domainLen := uint8(len(domain))
 	if domainLen > 255 {
 		return nil, fmt.Errorf("invalid domain length")
 	}
-	// var err error
 	// DST_prime = DST || I2OSP(len(DST), 1)
 	// b_0 = H(Z_pad || msg || l_i_b_str || I2OSP(0, 1) || DST_prime)
-	h.Reset()
-	h.Write(make([]byte, h.BlockSize()))
-	h.Write(msg)
-	h.Write([]byte{uint8(outLen >> 8), uint8(outLen)})
-	h.Write([]byte{0})
-	h.Write(domain)
-	h.Write([]byte{domainLen})
+	_, _ = h.Write(make([]byte, h.BlockSize()))
+	_, _ = h.Write(msg)
+	_, _ = h.Write([]byte{uint8(outLen >> 8), uint8(outLen)})
+	_, _ = h.Write([]byte{0})
+	_, _ = h.Write(domain)
+	_, _ = h.Write([]byte{domainLen})
 	b0 := h.Sum(nil)
 
 	// b_1 = H(b_0 || I2OSP(1, 1) || DST_prime)
 	h.Reset()
-	h.Write(b0)
-	h.Write([]byte{1})
-	h.Write(domain)
-	h.Write([]byte{domainLen})
+	_, _ = h.Write(b0)
+	_, _ = h.Write([]byte{1})
+	_, _ = h.Write(domain)
+	_, _ = h.Write([]byte{domainLen})
 	b1 := h.Sum(nil)
 
 	// b_i = H(strxor(b_0, b_(i - 1)) || I2OSP(i, 1) || DST_prime)
@@ -57,10 +55,10 @@ func expandMsg(h hash.Hash, msg []byte, domain []byte, outLen int) ([]byte, erro
 		for j := 0; j < h.Size(); j++ {
 			tmp[j] = b0[j] ^ bi[j]
 		}
-		h.Write(tmp)
-		h.Write([]byte{1 + uint8(i)})
-		h.Write(domain)
-		h.Write([]byte{domainLen})
+		_, _ = h.Write(tmp)
+		_, _ = h.Write([]byte{1 + uint8(i)})
+		_, _ = h.Write(domain)
+		_, _ = h.Write([]byte{domainLen})
 
 		// b_1 || ... || b_(ell - 1)
 		copy(out[(i-1)*h.Size():i*h.Size()], bi[:])
