4ee29ef972d9c24b458638cee875a71e85c91e7c
Antonio Sanso <antonio.sanso@gmail.com>
2023-05-08 09:30:39 +0200

    add test g2 initial

--- g2_test.go
+++ g2_test.go
@@ -6,7 +6,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/big"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	testDir                = "tests/bls"
+	deserializationG1Tests = filepath.Join(testDir, "deserialization_G1/*")
+	deserializationG2Tests = filepath.Join(testDir, "deserialization_G2/*")
 )
 
 func (g *G2) one() *PointG2 {
@@ -357,6 +366,43 @@ func TestZKCryptoVectorsG2CompressedValid(t *testing.T) {
 
 		g.Add(p1, p1, &g2One)
 	}
+}
+
+func TestZKCryptoVectorsG2Compressed(t *testing.T) {
+	type Test struct {
+		Input struct {
+			SignatureHexStr string `yaml:"signature"`
+		}
+		IsValidPredicate *bool `yaml:"output"`
+	}
+	tests, err := filepath.Glob(deserializationG2Tests)
+	require.NoError(t, err)
+	for _, testPath := range tests {
+		fmt.Println(testPath)
+		/*t.Run(testPath, func(t *testing.T) {
+			testFile, err := os.Open(testPath)
+			require.NoError(t, err)
+			test := Test{}
+			err = yaml.NewDecoder(testFile).Decode(&test)
+			require.NoError(t, testFile.Close())
+			require.NoError(t, err)
+			testCaseValid := test.IsValidPredicate != nil
+			byts, err := hex.DecodeString(test.Input.SignatureHexStr)
+			if err != nil && testCaseValid {
+				panic(err)
+			}
+
+			var point G2Affine
+			_, err = point.SetBytes(byts[:])
+			if err == nil && !testCaseValid {
+				panic("err should not be nil")
+			}
+			if err != nil && testCaseValid {
+				panic("err should be nil")
+			}
+		})*/
+	}
+
 }
 
 func TestG2MultiExpExpected(t *testing.T) {
